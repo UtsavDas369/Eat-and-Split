04f0059021a930f29d37ed0dc235d3c6b0cb5fff
Utsav Das <85437460+UtsavDas369@users.noreply.github.com>
2023-09-30 18:24:39 +0530

    Intial Commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Eat and Split</h1>
+      <                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    </div>
+  );
+}
+
+export default Home;
